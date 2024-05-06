2834d2a4e09dbe224962d8048cb2fbeb17acce77
KartikeySrivastava08 <146875256+KartikeySrivastava08@users.noreply.github.com>
2024-05-06 17:30:05 +0530

    Add files via upload

---
+++ Task-1/script.js
@@ -0,0 +1,20 @@
+let a = document.querySelector(".input");
+function add(x){
+    a.value += x;
+}
+function plus(){
+    const adds = "+";
+    a.value += adds ;
+}
+function minus(){
+    const subs = "-"
+    a.value += subs;
+}
+function mul(){
+    const muls = "*"
+    a.value += muls;
+}
+function div(){
+    const divs = "/"
+    a.value += divs;
+}
